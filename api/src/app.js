76420a24f2ca0f293bef808729c17e14e385fb5e
rtnkwami <nukukwami@outlook.com>
2025-06-05 14:46:50 +0000

    Added Auth0 auth checks

--- api/src/app.js
+++ api/src/app.js
@@ -1,10 +1,29 @@
+import dotenv from 'dotenv';
+dotenv.config()
+
 import express from 'express';
 import shopRoutes from './routes/shop.routes.js';
 const app = express();
+import { auth } from 'express-oauth2-jwt-bearer';
+import cors from 'cors';
 
+const checkJwt = auth({
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: process.env.AUTH0_ISSUER,
+  tokenSigningAlg: process.env.AUTH0_SIGINING_ALGORITHM
+});
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/shops', shopRoutes);
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "Correctly authenticated app!" });
+    // console.log(req.auth.payload.sub);
+})
 
 export default app;
--- api/src/server.js
+++ api/src/server.js
@@ -1,8 +1,23 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import app from './app.js'
-import { testDbConnection } from './config/dbConfig.js';
+import { testDbConnection, sequelize } from './config/dbConfig.js';
+import User from './models/User.js';
+import Shop from './models/Shop.js';
 
-app.listen(5000, () => {
-    console.log('Server is up and running...');
-});
+async function startServer() {
+  try {
+    await sequelize.sync({ alter: true });
+    console.log("Database synced");
+    
+    app.listen(3000, () => {
+      console.log("Server running on port 3000");
+    });
+  } catch (error) {
+    console.error("Error starting server:", error);
+  }
+}
+
+startServer();
 
 testDbConnection();
