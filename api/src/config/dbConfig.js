db022c4d72fbc5b5bdb69eeff310a82baf6c6eaa
rtnkwami <nukukwami@outlook.com>
2025-06-05 05:24:37 +0000

    Added a connection test to postgres

--- api/src/config/dbConfig.js
+++ api/src/config/dbConfig.js
@@ -2,12 +2,16 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_CONNECTION); // database needs to be created manually first
+export const sequelize = new Sequelize(process.env.DB_CONNECTION); // database needs to be created manually first
 
 export const testDbConnection = async () => {
     try {
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
+
+        await sequelize.sync();
+        console.log('All models synced.')
+
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
