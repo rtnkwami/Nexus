f9109694ec91520f1e03190caa8968df2df8a501
rtnkwami <nukukwami@outlook.com>
2025-06-05 14:49:07 +0000

    Moved database sync

--- api/src/config/dbConfig.js
+++ api/src/config/dbConfig.js
@@ -1,17 +1,22 @@
-import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
+import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize(process.env.DB_CONNECTION); // database needs to be created manually first
+export const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: 'localhost',
+        dialect: 'postgres',
+        logging: false
+    }
+) // database needs to be created manually first
 
 export const testDbConnection = async () => {
     try {
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
-
-        await sequelize.sync({ force: true });
-        console.log('All models synced.')
-
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
