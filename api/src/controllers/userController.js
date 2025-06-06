97a97dfb078502fac170dc56713ca3ac048a2907
rtnkwami <nukukwami@outlook.com>
2025-06-06 10:50:56 +0000

    Restructured user controller

--- api/src/controllers/userController.js
+++ api/src/controllers/userController.js
@@ -2,16 +2,14 @@ import User from '../models/User.js';
 
 export const getUser = async (req, res) => {
     const { sub, name } = req.body;
-    console.log(sub);
 
     try {
-        const user = await User.findOrCreate({
+        await User.findOrCreate({
             where: { auth0_uid: sub, name: name },
             defaults: { auth0_uid: sub, name: name}
         });
 
         res.status(200).json({ message: "User created or present" });
-        console.log(user.auth0_uid);
     } catch (error) {
         console.error(`Error getting or creating user: ${error}`);
     }
