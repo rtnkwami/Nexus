48f6cb223957cbd70e6c9cc6a3902ea6949b0116
rtnkwami <nukukwami@outlook.com>
2025-06-06 10:40:15 +0000

    Added function to create user if not present

---
+++ api/src/controllers/userController.js
@@ -0,0 +1,18 @@
+import User from '../models/User.js';
+
+export const getUser = async (req, res) => {
+    const { sub } = req.body;
+    console.log(sub);
+
+    try {
+        const user = await User.findOrCreate({
+            where: { auth0_uid: sub },
+            defaults: { auth0_uid: sub}
+        });
+
+        res.status(200).json({ message: "User created or present" });
+        console.log(user.auth0_uid);
+    } catch (error) {
+        console.error(`Error getting or creating user: ${error}`);
+    }
+}
