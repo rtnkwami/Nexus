731d25daf87b43711b0d421992abe4116ba74a90
rtnkwami <nukukwami@outlook.com>
2025-06-12 20:11:44 +0000

    Added an try catch block to addToCart

--- api/src/controllers/cartController.js
+++ api/src/controllers/cartController.js
@@ -1,27 +1,36 @@
 import Product from "../models/Product.js";
 
-export const addToCart = async (req, res) => {
-    const { id, quantity } = req.body;
+export const addToCart = async (req, res) => { 
+    try {
+        const { id, quantity } = req.body;
+    
+        if (!req.session.cart){
+            req.session.cart = [];
+        }
+    
+        const product = await Product.findByPk(id);
+        if (!product){
+            return res.status(400).json({ message: "Invalid product id" });
+        }
+    
+        const existingProduct = req.session.cart.find(item => item.id === id);
+        if (existingProduct) { 
+            existingProduct.quantity += quantity;
+    
+        } else {
+            req.session.cart.push({ id, quantity })
+    
+        }
+    
+        return res.json({ cart: req.session.cart });
 
-    if (!req.session.cart){
-        req.session.cart = [];
+    } catch (error){
+        console.error("Error adding product to cart: ", error);
+        return res.status(500).json({ message: "Internal server error" });
     }
+    
+}
 
-    const product = await Product.findByPk(id);
-    if (!product){
-        return res.status(400).json({ message: "Invalid product id" });
-    }
-
-    const existingProduct = req.session.cart.find(item => item.id === id);
-    if (existingProduct) { 
-        existingProduct.quantity += quantity
-
-    } else {
-        req.session.cart.push({ id, quantity })
-
-    }
-
-    res.json({ cart: req.session.cart });
-    console.log(req.session.id);
-
+export const updateCart = async (req, res) => {
+    
 }
