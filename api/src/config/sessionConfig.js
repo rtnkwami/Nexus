01579d889619e48d536ef3300605f697010de654
rtnkwami <nukukwami@outlook.com>
2025-06-15 15:52:30 +0000

    Added cart store to db

--- api/src/config/sessionConfig.js
+++ api/src/config/sessionConfig.js
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
 import session from 'express-session';
+import connectPgSimple from 'connect-pg-simple';
+import pg from 'pg';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -8,9 +10,22 @@ const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
+const pgSession = connectPgSimple(session);
+const pgPool = new pg.Pool({
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: 5432
+});
+
 export const appSession = session({
+    store: new pgSession({
+        pool: pgPool,
+        createTableIfMissing: true
+    }),
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUnitialised: false,
+    saveUnintialized: false,
     cookie: { secure: false, maxAge: 86400000 }
 });
