31f8f7543836b0e6efd8aab2ac42eb2f0a5970c2
rtnkwami <nukukwami@outlook.com>
2025-06-15 20:21:21 +0000

    Removed unneeded tables

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -1,8 +1,6 @@
 import User from "./User.js";
 import Shop from "./Shop.js";
 import Product from "./Product.js";
-import Cart from "./Cart.js";
-import CartItem from "./CartItem.js";
 import { sequelize } from "../config/dbConfig.js";
 
 
@@ -10,14 +8,8 @@ import { sequelize } from "../config/dbConfig.js";
 User.hasOne(Shop);
 Shop.belongsTo(User);
 
-User.hasOne(Cart);
-Cart.belongsTo(User);
-
 Shop.hasMany(Product);
 Product.belongsTo(Shop);
-
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
 
 
 export {
