bfdacc89950c8b5f453548974c9c95c2e3c9396f
rtnkwami <nukukwami@outlook.com>
2025-06-15 20:20:27 +0000

    Changed cart routes to use items

--- api/src/routes/cart.routes.js
+++ api/src/routes/cart.routes.js
@@ -1,14 +1,13 @@
 import express from 'express';
 import { appSession } from '../config/sessionConfig.js';
 import { addToCart, getCart, removeFromCart } from '../controllers/cart.controller.js';
-import { get } from 'http';
 
 const router = express.Router();
 
 router.use(appSession);
 
 router.get('/', getCart)
-router.post('/', addToCart);
-router.delete('/:id', removeFromCart)
+router.post('/items', addToCart);
+router.delete('/items/:id', removeFromCart)
 
 export default router;
