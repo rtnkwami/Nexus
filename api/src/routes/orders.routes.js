2623c8c8b3caac3dc5486990c28fbd2b2b58d6d2
rtnkwami <nukukwami@outlook.com>
2025-06-21 06:12:07 +0000

    Added a order routes for new controller

--- api/src/routes/orders.routes.js
+++ api/src/routes/orders.routes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import { appSession } from '../config/sessionConfig.js';
 import checkJwt from '../utils/verifyJwt.js';
-import { getAllOrders } from '../controllers/order.controller.js';
+import { getAllOrders, getOneOrder } from '../controllers/order.controller.js';
 
 const router = express.Router();
 
 router.use(appSession);
 
 router.get('/', checkJwt, getAllOrders);
+
+router.get('/:orderId', getOneOrder);
 
 export default router;
