01223124333cde1cd591ffec4390c9e5a4580863
rtnkwami <nukukwami@outlook.com>
2025-06-21 17:44:24 +0000

    Added a route to edit cart items

--- api/src/controllers/cart.controller.js
+++ api/src/controllers/cart.controller.js
@@ -54,11 +54,11 @@ export const getCart = async (req, res) => {
 
 export const removeFromCart = async (req, res) => {
     try {
-        const { id } = req.params;
+        const { productId } = req.params;
 
-        const product = req.session.cart.find(item => item.id === id);
+        const product = req.session.cart.find(item => item.id === productId);
         if (product) {
-            req.session.cart = req.session.cart.filter(item => item.id !== id);
+            req.session.cart = req.session.cart.filter(item => item.id !== productId);
             
             return res.status(200).json({ 
                 message: "Product removed from cart successfully",
@@ -71,6 +71,38 @@ export const removeFromCart = async (req, res) => {
 
     } catch (error) {
         console.error("Error removing product from cart: ", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+export const editCartItem = async (req, res) => {
+    try {
+        const { productId } = req.params;
+        const { quantity } = req.body;
+
+        if (!req.session.cart) {
+            return res.status(400).json({ message: "Cart is empty" });
+        }
+
+        const product = req.session.cart.find(item => item.id === productId);
+        if (!product) {
+            return res.status(404).json({ message: "Product not found in cart" });
+        }
+
+        if (quantity <= 0) {
+            return res.status(400).json({ message: "Quantity must be greater than zero" });
+        }
+
+        product.quantity = quantity;
+
+        return res.status(200).json({ 
+            message: "Cart item updated successfully",
+            updatedCartItem: product,
+            cart: req.session.cart
+        });
+
+    } catch (error) {
+        console.error("Error editing cart item: ", error);
         return res.status(500).json({ message: "Internal server error" });
     }
 }
--- api/src/routes/cart.routes.js
+++ api/src/routes/cart.routes.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { appSession } from '../config/sessionConfig.js';
-import { addToCart, getCart, removeFromCart } from '../controllers/cart.controller.js';
+import { addToCart, getCart, removeFromCart, editCartItem } from '../controllers/cart.controller.js';
 
 const router = express.Router();
 
 router.use(appSession);
 
 router.get('/', getCart);
-router.post('/items', addToCart);
-router.delete('/items/:id', removeFromCart);
+router.post('/', addToCart);
+router.put('/:productId', editCartItem);
+router.delete('/:productId', removeFromCart);
 
 export default router;
