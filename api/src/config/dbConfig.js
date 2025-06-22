28c90ece42b27a1c5d87ba94d80cbe6241f3a73d
rtnkwami <nukukwami@outlook.com>
2025-06-22 14:36:12 +0000

    Changed db host

--- api/src/config/dbConfig.js
+++ api/src/config/dbConfig.js
@@ -15,7 +15,7 @@ export const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: 'localhost',
+        host: process.env.DB_HOST,
         dialect: 'postgres',
         logging: false
     }
