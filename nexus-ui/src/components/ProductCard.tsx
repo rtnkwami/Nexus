244e3f0b6fbb6ea7ad17d148cce2b132032f1457
rtnkwami <nukukwami@outlook.com>
2025-07-07 20:38:26 +0000

    fix(ui/product-images): added back the image component to view product images in homepage

--- nexus-ui/src/components/ProductCard.tsx
+++ nexus-ui/src/components/ProductCard.tsx
@@ -1,23 +1,23 @@
 import { Card, CardTitle, CardDescription } from "@/components/ui/card"
-// import Image from "next/image"
+import Image from "next/image"
 import { Product } from "@/types"
 
 export default function ProductCard({ product }: { product: Product }) {
   return (
     <Card className="w-full max-w-xs p-3">
-        <div className="relative w-full h-36 mb-3">
-            {/* <Image
-            src={product.imageUrl}
-            alt={product.name}
-            fill
-            className="rounded-md object-cover"
-            /> */}
+        <div className="relative w-full aspect-[4/3] mb-3">
+            <Image
+              src={product.images[0]}
+              alt={product.name}
+              fill
+              className="rounded-md object-cover"
+            />
         </div>
         <CardTitle className="text-base mb-1">{product.name}</CardTitle>
         <CardDescription className="text-sm text-muted-foreground mb-1">
             {product.description ?? "No description available."}
         </CardDescription>
-        <p className="text-lg font-medium">${product.price.toFixed(2)}</p>
+        <p className="text-lg font-medium">&#8373;{product.price.toFixed(2)}</p>
     </Card>
   )
 }
