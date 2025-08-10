67ca6fd79b60135a33c5e6cd5feb306593ed9ab7
rtnkwami <nukukwami@outlook.com>
2025-08-10 21:58:12 +0000

    feat(api/sales-performance): added service function to get sales performance over time

--- api/src/services/analytics.service.js
+++ api/src/services/analytics.service.js
@@ -1,5 +1,7 @@
 import { getOverviewDashboard } from "./analytics/dashboards/overview.js";
+import { getSalesPerformanceDashboard } from "./analytics/dashboards/SalesPerformance.js";
 
 export const Analytics = {
-    getOverviewDashboard
+    getOverviewDashboard,
+    getSalesPerformanceDashboard,
 };
