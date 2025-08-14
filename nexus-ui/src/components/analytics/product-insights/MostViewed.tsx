0890882841418aa7e1581c171c68ea74606fa7ca
rtnkwami <nukukwami@outlook.com>
2025-08-14 14:00:53 +0000

    chore(ui/product-insights): rewrote dashboard data access to use new api return object structure

--- nexus-ui/src/components/analytics/product-insights/MostViewed.tsx
+++ nexus-ui/src/components/analytics/product-insights/MostViewed.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent } from "@/components/ui/card";
 
-export default function MostViewedCard({ rankings }) {
-  if (!rankings || rankings.length === 0) {
+export default function MostViewedCard({ mostViewedProduct }) {
+  if (!mostViewedProduct) {
     return (
       <Card className="p-4">
         <CardContent>
@@ -13,18 +13,16 @@ export default function MostViewedCard({ rankings }) {
     );
   }
 
-  const topProduct = rankings[0];
-
   return (
     <Card className="p-4">
       <CardContent className="flex flex-col space-y-2">
         <p className="text-sm text-muted-foreground">Most Viewed Product</p>
 
-        <h2 className="text-lg font-semibold">{topProduct.product_name}</h2>
-        <p className="text-sm text-gray-500">Category: {topProduct.product_category}</p>
+        <h2 className="text-lg font-semibold">{mostViewedProduct.product_name}</h2>
+        {/* <p className="text-sm text-gray-500">Category: {topProduct.product_category}</p> */}
 
         <p className={`text-2xl font-bold`}>
-          {topProduct.view_count} views
+          {mostViewedProduct.view_count} views
         </p>
       </CardContent>
     </Card>
--- nexus-ui/src/components/analytics/product-insights/TopConversion.tsx
+++ nexus-ui/src/components/analytics/product-insights/TopConversion.tsx
@@ -7,8 +7,8 @@ function getConversionRateColor(rate: number) {
   return "text-green-600"; // Good
 }
 
-export default function TopConversionCard({ rankings }) {
-  if (!rankings || rankings.length === 0) {
+export default function TopConversionCard({ highestConversionProduct }) {
+  if (!highestConversionProduct) {
     return (
       <Card className="p-4">
         <CardContent>
@@ -20,16 +20,15 @@ export default function TopConversionCard({ rankings }) {
     );
   }
 
-  const topProduct = rankings[0];
-  const rate = parseFloat(topProduct.conversion_rate);
+  const rate = parseFloat(highestConversionProduct.conversion_rate);
 
   return (
     <Card className="p-4">
       <CardContent className="flex flex-col space-y-2">
         <p className="text-sm text-muted-foreground">Highest Conversion Rate</p>
 
-        <h2 className="text-lg font-semibold">{topProduct.product_name}</h2>
-        <p className="text-sm text-gray-500">Category: {topProduct.product_category}</p>
+        <h2 className="text-lg font-semibold">{highestConversionProduct.product_name}</h2>
+        {/* <p className="text-sm text-gray-500">Category: {topProduct.product_category}</p> */}
 
         <p className={`text-2xl font-bold ${getConversionRateColor(rate)}`}>
           {rate.toFixed(2)}%
