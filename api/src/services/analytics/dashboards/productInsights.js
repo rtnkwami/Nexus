735f4c476e60736172be110b4166f7ce5fc5dbba
rtnkwami <nukukwami@outlook.com>
2025-08-14 13:26:48 +0000

    feat(api/product-insights): added a metric for most viewed product to product insights dashboard

--- api/src/services/analytics/dashboards/productInsights.js
+++ api/src/services/analytics/dashboards/productInsights.js
@@ -3,9 +3,11 @@ import { getLineGraphDateRange } from "../../../utils/getDateRange.js";
 
 export const getProductInsightsDashboard = async (shopId, fromDate, toDate) => {
     const conversionRankings = await getHighestConversionProducts(shopId, fromDate, toDate);
+    const viewRankings = await getMostViewedProducts(shopId, fromDate, toDate);
 
     return {
-        conversionRankings
+        conversionRankings,
+        viewRankings
     }
 };
 
@@ -76,6 +78,50 @@ const getHighestConversionProducts = async (shopId, fromDate, toDate, ranking = 
 
     } catch (error) {
         console.error("Error fetching highest conversion products:", error);
+        return [];
+    }
+}
+
+
+const getMostViewedProducts = async (shopId, fromDate, toDate, ranking = 'highest', limit = 5) => {
+    try {
+        const { startDate, endDate } = getLineGraphDateRange(fromDate, toDate);
+
+        const order =
+            ranking?.toLowerCase() === 'highest'
+                ? 'DESC'
+                : ranking?.toLowerCase() === 'lowest'
+                ? 'ASC'
+                : 'DESC';
+
+        const query  = `
+            SELECT 
+                p.id as product_id,
+                p.name as product_name,
+                p.category as product_category,
+                COUNT(pv.id) as view_count
+            FROM "Products" p
+            INNER JOIN "ProductViews" pv ON p.id = pv."ProductId"
+            WHERE p."ShopId" = :shopId
+                AND pv."viewedAt" BETWEEN :startDate AND :endDate
+            GROUP BY p.id, p.name
+            ORDER BY view_count ${ order }
+            LIMIT ${ limit };
+        `;
+
+        const mostViewedProducts = await sequelize.query(query, {
+                type: sequelize.QueryTypes.SELECT,
+                replacements: {
+                    shopId,
+                    startDate,
+                    endDate
+                }
+        });
+
+        return mostViewedProducts;
+
+    } catch (error) {
+        console.error("Error fetching most viewed products:", error);
         return [];
     }
 }
